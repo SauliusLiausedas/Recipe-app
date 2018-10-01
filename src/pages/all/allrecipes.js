db0995227f2d7c3b3d01d5ae4f1be7a8d37decbd
Mindaugas <mindgs.v@gmail.com>
2018-10-01 19:44:15 +0300

    fix show recipes popup

--- src/pages/all/allrecipes.js
+++ src/pages/all/allrecipes.js
@@ -16,18 +16,6 @@ class AllRecipes extends Component {
         }
     }
 
-    showAllRecipes() {
-        for (let i=0; i<this.state.recipes.meal.length; i++) {
-            this.recipeIngredients[this.state.recipes.meal[i].name] = []
-            this.recipeNames.push(this.state.recipes.meal[i].name)
-            this.recipeMethod.push(this.state.recipes.meal[i].method)
-            this.recipeImg.push(this.state.recipes.meal[i].image)
-            for (let j = 0; j < this.state.recipes.meal[i].ingredients.length; j++) {
-                this.recipeIngredients[this.state.recipes.meal[i].name].push(this.state.recipes.meal[i].ingredients[j])
-            }
-        }
-    }
-
     viewRecipe(e) {
         this.setState({recipeToShow: e.currentTarget.id})
     }
@@ -35,14 +23,13 @@ class AllRecipes extends Component {
     render() {
         return (
             <div className="allRecipes">
-                {this.showAllRecipes()}
-                <ViewRecipe showRecipe={() => {this.recipeToRender()}} view={this.state.recipeToShow}/>
+                {this.state.recipeToShow ? (<ViewRecipe view={this.state.recipes.meal[this.state.recipeToShow]}/>) : ''}
                 <div className="boxes">
-                    {this.recipeNames.map((name, i) =>
+                    {this.state.recipes.meal.map((mealObj, i) =>
                         <div key={i} id={i} className="recipe-box" onClick={(e)=>this.viewRecipe(e)}>
-                            <img className="recipe-img" alt={name} src={this.recipeImg[i]}/>
-                            <h2 key={i}>{name}</h2>
-                            <em><p>{this.recipeMethod[i].slice(0, 250) + "..."}</p></em>
+                            <img className="recipe-img" alt={mealObj.name} src={mealObj.image}/>
+                            <h2 key={i}>{mealObj.name}</h2>
+                            <em><p>{mealObj.method.slice(0, 250) + "..."}</p></em>
                         </div>
                     )}
                 </div>
