ba5c5e1c3a21ce23455e929174dc0d16ca957da3
kentaras <kentaraz@gmail.com>
2018-10-01 18:13:05 +0300

    Starting page for all recipes

--- src/pages/all/view.js
+++ src/pages/all/view.js
@@ -49,13 +49,12 @@ class ViewRecipe extends Component {
         } else {
             return (
                 <div onClick={() => this.recipeToRender(this.props.view)}>
-                    {/*{this.props.showRecipe(this.props.view)}*/}
                     <div className={this.state.popup}>
                         <h2>{this.state.renderView.name}</h2>
                         <ul className="ingredients-ul">
                             {this.state.renderView.ingredients.map((ingredient, i)=> <li key={i} className="ingredients-li">{ingredient} </li> )}
                         </ul>
-                        <img className="recipeImg" src={this.state.renderView.image}/>
+                        <img alt={this.state.renderview.name} className="recipeImg" src={this.state.renderView.image}/>
                         <p className="method-text">{this.state.renderView.method}</p>
                     </div>
                 </div>
