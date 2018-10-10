8da416c09d4c14f82c0993dea41019030feaa5c5
valiunaite <gerda.valiunaite@gmail.com>
2018-10-10 09:50:45 +0300

    added button to homepage

--- src/body/recipes.js
+++ src/body/recipes.js
@@ -6,44 +6,33 @@ class Recipes extends Component {
         super(props)
         this.state = {
             recipes: window.recipeDB,
-            antraste: 'Labadiena'
+            recipesId: []
         }
 
-        this.myIndexesToShow = [];
-        let randomNumber = Math.floor(Math.random() * 9);
-        for (let i = 0; i < randomNumber; i++) {
-            let myNewRandomNumber = Math.floor(Math.random() * (this.state.recipes.meal.length - 1));
-            let isMyRandomNumberInRandomArray = this.myIndexesToShow.indexOf(myNewRandomNumber) !== -1;
-            if (isMyRandomNumberInRandomArray) {
-                i--;
-            } else {
-                this.myIndexesToShow.push(myNewRandomNumber);
-            }
-        }
+        this.state.recipesId = this.getMyIndexesToShow();
 
-        this.getRandomObjects = this.getRandomObjects.bind(this);
-        this.getRender = this.getRender.bind(this);
-//        this.showNewRender = this.showNewRender.bind(this);
         this.handleClick = this.handleClick.bind(this);
     }
 
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    componentDidMount() {
-        document.getElementById('kazkoksId');
-    }
-
-//    componentWillMount() {
-//        this.setState({
-//            recipes: this.getRandomObjects()
-//        })
-//    }
+    getMyIndexesToShow() {
+        let myIndexesToShow = [];
+        let randomNumber = Math.floor(Math.random() * 9);
+        for (let i = 0; i < randomNumber; i++) {
+            let myNewRandomNumber = Math.floor(Math.random() * (this.state.recipes.meal.length - 1));
+            let isMyRandomNumberInRandomArray = myIndexesToShow.indexOf(myNewRandomNumber) !== -1;
+            if (isMyRandomNumberInRandomArray) {
+                i--;
+            } else {
+                myIndexesToShow.push(myNewRandomNumber);
+            }
+        }
         
+        return myIndexesToShow;
+    }
+      
     getRandomObjects() {
         return this.state.recipes.meal.filter((mealObj, index) => {
-                return this.myIndexesToShow.indexOf(index) !== -1
+                return this.state.recipesId.indexOf(index) !== -1
             }
         )
     }
@@ -62,35 +51,25 @@ class Recipes extends Component {
         </div>)
     }
 
-    handleClick() {
-
-        // this.setState({
-        //     recipes: this.getRandomObjects().map((mealObj, i) => this.getRender(mealObj, i))
-        // })
-
-        console.log('veikia')
-        this.setState({antraste: 'Visogero'})
-    }
-
     showNewRender(){
-        return this.getRandomObjects().map((mealObj, i) => this.getRender(mealObj, i))
+        return this.getRandomObjects().map((mealObj, i) => this.getRender(mealObj, i));
     }
 
+    handleClick() {
+        this.setState({
+          recipesId: this.getMyIndexesToShow()
+        });
+      }
+      
     render() {
         return (
             <div>
-                {(() => {
-                    return this.getRandomObjects().map((mealObj, i) => this.getRender(mealObj, i))
-                })()}
                 <button onClick={this.handleClick}>
-                    Recipes: {this.myIndexesToShow.length}
+                    Recipes: {this.state.recipesId.length}
                 </button>
-                <button onClick={this.handleClick()}>
-                    Recipes: {this.myIndexesToShow.length}
-                </button>
-                <h1>{this.state.antraste}</h1>
-                <div></div>
+                <div>
                     {this.showNewRender()}
+                </div>
             </div>
         )
     }
