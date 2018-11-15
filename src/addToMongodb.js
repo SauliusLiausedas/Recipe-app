cd251c2e406afe91ed1be5f76c44791848b895c6
Mindaugas <mindgs.v@gmail.com>
2018-11-15 16:31:22 +0200

    get all categories from db

--- src/addToMongodb.js
+++ src/addToMongodb.js
@@ -11,7 +11,7 @@ class AddToMongodb extends Component {
 
     addToMongodb() {
         getRandomRecipes(1).then((data) => {
-            console.log(fetch('https://recipe-app-mindgsv244709.codeanyapp.com/insertfrommealdb', {
+            console.log(fetch('https://recipe-app-mindgsv244709.codeanyapp.com:2000/insertfrommealdb', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
@@ -55,7 +55,7 @@ class AddToMongodb extends Component {
     addCategoriesToMongo() {
         getCategories().then(categories => {
             categories.categories.forEach(category => {
-                console.log(fetch('https://recipe-app-mindgsv244709.codeanyapp.com/insertcategories', {
+                console.log(fetch('https://recipe-app-mindgsv244709.codeanyapp.com:2000/insertcategories', {
                     method: 'POST',
                     headers: {
                         'Accept': 'application/json',
--- src/mongoservice.js
+++ src/mongoservice.js
@@ -1,6 +1,6 @@
 export default class {
     static getAllRecipes (itemsPerPage, page) {
-         return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/getallrecipes/${itemsPerPage}/${page}`, {
+         return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/getallrecipes/${itemsPerPage}/${page}`, {
         }).then(response => {
             return response.json().then((data=>{
             return data;
@@ -8,7 +8,7 @@ export default class {
     }
 
     static getRecipesCount () {
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/getrecipescount/`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/getrecipescount/`, {
         }).then(response => {
             return response.json().then((data=>{
                 return data;
@@ -16,7 +16,7 @@ export default class {
     }
 
    static getSearchValue (searchValue, itemsPerPage, page) {
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/searchrecipe/${searchValue}/${itemsPerPage}/${page}`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/searchrecipe/${searchValue}/${itemsPerPage}/${page}`, {
         }).then(response => {
             return response.json().then((data=>{
                 return data;
@@ -24,7 +24,7 @@ export default class {
     }
 
     static getRecipeById (id) {
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/getById/${id}`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/getById/${id}`, {
         }).then(response => {
             return response.json().then((data=>{
                 return data;
@@ -33,7 +33,7 @@ export default class {
 
     static createRecipe (recipe) {
         console.log(recipe);
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/insertfrommealdb`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/insertfrommealdb`, {
             method: 'POST',
             body: JSON.stringify(recipe),
             headers: {
@@ -48,7 +48,7 @@ export default class {
 
     static updateRecipe (recipe) {
         console.log(recipe);
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/updateRecipe`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/updateRecipe`, {
             method: 'PUT',
             body: JSON.stringify(recipe),
             headers: {
@@ -60,7 +60,7 @@ export default class {
     }
 
     static getCategories () {
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/getCategories/`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/getCategories/`, {
         }).then(response => {
             return response.json().then((data=>{
                 return data;
@@ -68,7 +68,7 @@ export default class {
     }
 
     static getRecipesByCategory (category) {
-        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com/getRecipesByCategory/${category}`, {
+        return fetch(`https://recipe-app-mindgsv244709.codeanyapp.com:2000/getRecipesByCategory/${category}`, {
         }).then(response => {
             return response.json().then((data=>{
                 return data;
